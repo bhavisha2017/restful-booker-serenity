ca9c96b648e0d328967e05423aa942ef60a103f9
bhavisha2017 <yellowjasmine786@gmail.com>
2023-06-05 22:23:59 +0100

    Initial commit

---
+++ src/test/java/com/retful/booker/testbase/TestBase.java
@@ -0,0 +1,23 @@
+package com.retful.booker.testbase;
+
+
+import com.retful.booker.utils.PropertyReader;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+
+public class TestBase {
+    public static PropertyReader propertyReader;
+
+
+
+    @BeforeClass
+    public static void inIt() {
+        propertyReader = PropertyReader.getInstance();
+        RestAssured.baseURI = propertyReader.getProperty("baseUrl");
+
+
+    }
+
+
+}
